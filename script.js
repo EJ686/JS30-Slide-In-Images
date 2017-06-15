e20583590a7183871a3bd4c414ea1111bda3a7d0
Edgar Juarez <ejjt.686@gmail.com>
2017-06-14 17:48:43 -0700

    Images now slide in when 50% of image is shown as well when scrolling up

--- script.js
+++ script.js
@@ -16,7 +16,21 @@ function debounce(func, wait = 20, immediate = true) {
 const sliderImages = document.querySelectorAll('.slide-in');
 
 function checkSlide(e) {
-    console.log(e);
+    sliderImages.forEach(sliderImage => {
+        // Half way through the image
+        const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+        // Bottom of the image
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+
+        if (isHalfShown && isNotScrolledPast) {
+            sliderImage.classList.add('active');
+        } else {
+            sliderImage.classList.remove('active');
+        }
+
+    });
 };
 
 window.addEventListener('scroll', debounce(checkSlide));
